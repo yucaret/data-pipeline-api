0feb1ae3d242c6def19aeb4f5146154713364987
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-07-05 19:03:47 -0500

    Add files via upload

--- app/database.py
+++ app/database.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+print("Conexión a base de datos:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
