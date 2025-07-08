dc3b8fab97bb65341fa907253c67a65a5c3607c0
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-07-08 01:01:09 -0500

    Add files via upload

--- app/crud.py
+++ app/crud.py
@@ -62,9 +62,8 @@ def validate_and_convert_dataframe(df: pd.DataFrame, type_: str, has_header: boo
         
         elif dtype == "datetime":
             try:
-                print("crud.py --> validate_and_convert_dataframe --> elif dtype == datetime: --> df:" + str(df[df[col_name].isnull()]))
+                #df[col_name] = pd.to_datetime(df[col_name], errors='coerce')
                 
-                df[col_name] = pd.to_datetime(df[col_name], errors='coerce')
                 print("crud.py --> validate_and_convert_dataframe --> df[col_name] = pd.to_datetime(df[col_name]) --> df:" + str(df[df[col_name].isnull()]))
                 
                 df[col_name] = df[col_name].where(df[col_name].notna(), None)
