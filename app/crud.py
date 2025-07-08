83b37f194b110bd293ab3dae568c26701fb18a22
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-07-08 00:05:45 -0500

    Add files via upload

--- app/crud.py
+++ app/crud.py
@@ -63,6 +63,7 @@ def validate_and_convert_dataframe(df: pd.DataFrame, type_: str, has_header: boo
         elif dtype == "datetime":
             try:
                 df[col_name] = pd.to_datetime(df[col_name])
+                df[col_name] = df[col_name].where(df[col_name].notnull(), None)
             except Exception:
                 raise ValueError(f"No se pudo convertir '{col_name}' a datetime.")
 
