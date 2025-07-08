ec0cb3ebe8aad8bd34dd5aa22d5e305551f7fbf1
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-07-07 22:51:24 -0500

    Add files via upload

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -9,6 +9,10 @@ router = APIRouter()
 @router.post("/upload")
 async def upload_file(type_: str = Form(...), file: UploadFile = File(...), has_header: bool = Form(True)):
 
+    print("upload.py --> upload_file --> type_: " + str(type_) + "; file: " + str(file) + "; has_header: " + str(has_header))
+    
+    print("upload.py --> upload_file --> list(tables_metadata.keys()): " + str(list(tables_metadata.keys())))
+
     if type_ not in list(tables_metadata.keys()):
         raise HTTPException(status_code=400, detail=f"Tabla no reconocida. Usar uno de: {list(tables_metadata.keys())}")
 
@@ -16,11 +20,15 @@ async def upload_file(type_: str = Form(...), file: UploadFile = File(...), has_
         df = pd.read_csv(file.file, header=0 if has_header else None)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error leyendo CSV: {str(e)}")
+        
+    print("upload.py --> upload_file --> df = pd.read_csv(file.file, header=0 if has_header else None)")
 
     try:
         objects = validate_and_convert_dataframe(df, type_, has_header)
     except ValueError as ve:
         raise HTTPException(status_code=400, detail=str(ve))
+        
+    print("upload.py --> upload_file --> objects = validate_and_convert_dataframe(df, type_, has_header)")
 
     try:
         insert_objects_in_db(objects)
@@ -28,5 +36,7 @@ async def upload_file(type_: str = Form(...), file: UploadFile = File(...), has_
         raise HTTPException(status_code=400, detail="Error: claves duplicadas o violacion de integridad.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error interno: {str(e)}")
+        
+    print("upload.py --> upload_file --> insert_objects_in_db(objects)")
 
     return {"message": f"{len(objects)} registros insertados en {type} exitosamente."}
